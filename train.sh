04f11903e8912aae60a960751ef7f44ff6127aff
Claudio Cusano <claudio.cusano@unipv.it>
2018-06-15 16:43:23 +0200

    update training script

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DB=/mnt/ssd/dataset/fivek
+
+
+python3 main.py \
+	-i $DB/raw \
+	-e $DB/c \
+	-bs 50 -np 10 -nf 8 \
+	-tr $DB/train_mit.txt $DB/test_mit_random250.txt \
+	-en 2018-06-14-baseline-de94
