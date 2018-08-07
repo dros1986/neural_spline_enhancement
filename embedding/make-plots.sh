7ab1401159294e3b77c0a3f5424862ab4fd51a28
Claudio Cusano <claudio.cusano@unipv.it>
2018-08-07 22:05:33 +0200

    New plots

--- embedding/make-plots.sh
+++ embedding/make-plots.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
-for S in vote fit; do
-    LINE=4
-    for e in a b c d e; do
-	DATA="$S-profile-results-$e.txt"
-	SUMMARY="$S-profile-summary-$e.txt"
-	PDF="$S-profile-$e.pdf"
-	CROP=${PDF%.pdf}-crop.pdf
 
-	# REF is the performance obtained learning from the expert
-	REF=$(head -$LINE noprofiles.txt | tail -1 | cut -d' ' -f 1)
-	LINE=$(( $LINE + 1 ))
+# Both strategies on the same plot
+LINE=4
+for e in a b c d e; do
+    DATA1="vote-profile-results-$e.txt"
+    DATA2="fit-profile-results-$e.txt"
+    SUMMARY1="vote-profile-summary-$e.txt"
+    SUMMARY2="fit-profile-summary-$e.txt"
+    PDF="profile-$e.pdf"
+    CROP=${PDF%.pdf}-crop.pdf
 
-	# Creaty a summary from the raw data
-	grep AVG -A2 $DATA | tr $'\n' ' ' | sed "s/--/\n/g" > $SUMMARY
+    # REF is the performance obtained learning from the expert
+    REF=$(head -$LINE noprofiles.txt | tail -1 | cut -d' ' -f 1)
+    LINE=$(( $LINE + 1 ))
 
-	# Make the plot
-	gnuplot -c profile.plot "Expert ${e^^}" $SUMMARY $REF > $PDF
+    # Creaty summaries from the raw data
+    grep AVG -A2 $DATA1 | tr $'\n' ' ' | sed "s/--/\n/g" > $SUMMARY1
+    grep AVG -A2 $DATA2 | tr $'\n' ' ' | sed "s/--/\n/g" > $SUMMARY2
 
-	# Crop the result
-	pdfcrop $PDF
-	mv $CROP $PDF
-    done
+    # Make the plot
+    gnuplot -c profile2.plot "Expert ${e^^}" $SUMMARY1 $SUMMARY2 $REF > $PDF
+
+    # Crop the result
+    pdfcrop $PDF
+    mv $CROP $PDF
 done
+
+
+# One plot per strategy
+# for S in vote fit; do
+#     LINE=4
+#     for e in a b c d e; do
+# 	DATA="$S-profile-results-$e.txt"
+# 	SUMMARY="$S-profile-summary-$e.txt"
+# 	PDF="$S-profile-$e.pdf"
+# 	CROP=${PDF%.pdf}-crop.pdf
+
+# 	# REF is the performance obtained learning from the expert
+# 	REF=$(head -$LINE noprofiles.txt | tail -1 | cut -d' ' -f 1)
+# 	LINE=$(( $LINE + 1 ))
+
+# 	# Creaty a summary from the raw data
+# 	grep AVG -A2 $DATA | tr $'\n' ' ' | sed "s/--/\n/g" > $SUMMARY
+
+# 	# Make the plot
+# 	gnuplot -c profile.plot "Expert ${e^^}" $SUMMARY $REF > $PDF
+
+# 	# Crop the result
+# 	pdfcrop $PDF
+# 	mv $CROP $PDF
+#     done
+# done
 
 
 
---
+++ embedding/profile2.plot
@@ -0,0 +1,23 @@
+# ARG1: title
+# ARG2: first data file
+# ARG3: second data file
+# ARG4: reference performance
+
+set size 0.5, 0.75
+set terminal pdf enhanced
+set termoption dash
+set logscale x
+set xlabel "# images"
+set ylabel "{/Symbol D\}E_{76}"
+set title ARG1
+
+set ytics 2
+# set yrange [8:14]
+
+reference=ARG4 + 0
+
+plot ARG2 using 2:18:28 with filledcurve fc rgb "#C0000080" notitle, \
+     ARG3 using 2:18:28 with filledcurve fc rgb "#C0F07000" notitle, \
+     ARG2 using 2:8 with linespoints title "Voting" lc rgb "#000080" lw 2 pt 7 ps 0.6, \
+     ARG3 using 2:8 with linespoints title "Fitting" lc rgb "#F07000" lw 2 pt 13 ps 0.6, \
+     reference with lines title "Training" lc rgb "black" dt 2 lw 2
